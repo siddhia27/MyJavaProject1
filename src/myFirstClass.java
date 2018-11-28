c4bb0d490882a5defbc175ed363a5844ad409a3a
Siddhi Agrawal <SIDDHIA@amdocs.com>
2018-11-28 15:55:53 +0530

    first commit

---
+++ src/myFirstClass.java
@@ -0,0 +1,18 @@
+
+public class myFirstClass {
+	
+	public static void main(String args[]){  
+		
+		String q = "";
+	     System.out.println("Hello Java"); 
+	     
+	     try {
+			String p = "Hello Java";
+			System.out.println(p); 
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("cannot print the value of string");
+		}
+	    }  
+}
